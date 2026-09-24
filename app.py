299c88eeb36e348c0b8b620da08fda063acfb946
Many-Vasquez <jose_vasquez_moyeda@hotmail.com>
2026-09-24 16:48:30 -0600

    Refactor data frame creation and saving logic

--- app.py
+++ app.py
@@ -854,16 +854,10 @@ elif menu == "🏢 Catálogo Puntos de Trabajo":
                     ARCHIVO_PUNTOS, ["NombrePunto", "Ubicacion", "FechaAlta"]
                 )
 
-                nueva_fila = pd.DataFrame([{
-				"NombrePunto": nombre_punto, 
-				"Ubicacion": ubicacion, 
-				"FechaAlta": fecha_alta
-				}])
-
-                puntos = pd.concat([puntos, nueva_fila], ignore_index=True),
-                guardar_datos(puntos, ARCHIVO_PUNTOS)
+nueva_fila = pd.DataFrame([{"NombrePunto": nombre_punto, "Ubicacion": ubicacion, "FechaAlta": fecha_alta}])
+        puntos = pd.concat([puntos, nueva_fila], ignore_index=True)
+        guardar_datos(puntos, ARCHIVO_PUNTOS)
         st.success("¡Punto de trabajo guardado exitosamente!")
-                ])
 				
                 guardar_datos(
                     ARCHIVO_PUNTOS,
